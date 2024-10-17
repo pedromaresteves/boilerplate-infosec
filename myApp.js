3ac6e5f7ca9779b3d7cceaf07d6ee370e895e5aa
Pedro Esteves <pedromaresteves@gmail.com>
2024-10-17 21:37:06 +0100

    Deny iframing

--- myApp.js
+++ myApp.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const helmet = require("helmet");
-app.use(helmet.hidePoweredBy());
 
+app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({ action: "deny" }));
 
 
 
