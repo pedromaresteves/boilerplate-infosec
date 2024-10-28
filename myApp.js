bc6065a9baf1b15361e599f6683108fa8e8a6907
pedromaresteves <pedromaresteves@gmail.com>
2024-10-28 21:28:44 +0000

    Switch previous lines for a helmet config obj

--- myApp.js
+++ myApp.js
@@ -3,17 +3,27 @@ const app = express();
 const helmet = require("helmet");
 const ninetyDaysInSeconds = 90 * 24 * 60 * 60;;
 
-app.use(helmet.hidePoweredBy()); //Hide this the app is powered by Express
-app.use(helmet.frameguard({ action: "deny" })); //Deny this app can be used in an iframe
-app.use(helmet.xssFilter()); //?Reduces? XSS attacks
-app.use(helmet.noSniff());  //Instructs the browser to not bypass the provided Content-Type
-app.use(helmet.ieNoOpen());
-app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true })); //Redirect http to https
-app.use(helmet.dnsPrefetchControl()); //Disable DNS Prefetching
-app.use(helmet.noCache()); //Disable browser caching
-app.use(helmet.contentSecurityPolicy({ directives: { defaultSrc: ["'self'"], scriptSrc: ["'self'", "trusted-cdn.com"] } })); //A powerful allow-list of what can happen on your page which mitigates many attacks
+// app.use(helmet.hidePoweredBy()); //Hide this the app is powered by Express
+// app.use(helmet.frameguard({ action: "deny" })); //Deny this app can be used in an iframe
+// app.use(helmet.xssFilter()); //?Reduces? XSS attacks
+// app.use(helmet.noSniff());  //Instructs the browser to not bypass the provided Content-Type
+// app.use(helmet.ieNoOpen());
+// app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true })); //Redirect http to https
+// app.use(helmet.dnsPrefetchControl()); //Disable DNS Prefetching
+// app.use(helmet.noCache()); //Disable browser caching
+// app.use(helmet.contentSecurityPolicy({ directives: { defaultSrc: ["'self'"], scriptSrc: ["'self'", "trusted-cdn.com"] } })); //A powerful allow-list of what can happen on your page which mitigates many attacks
 
-
+app.use(helmet({
+  frameguard: {
+    action: "deny"
+  }, contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "trusted-cdn.com"],
+    },
+    dnsPrefetchControl: false
+  },
+}))
 
 
 
